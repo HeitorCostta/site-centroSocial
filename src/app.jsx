3273d754efd96a43b8bacfc353f644bf854d1aa2
HeitorCostta <heitor.fdcosta@gmail.com>
2026-05-08 20:16:29 -0300

    fix app filename

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Navbar from "./components/Navbar/Navbar";
-import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar.jsx";
+import Footer from "./components/Footer/Footer.jsx";
 
 import Home from "./pages/Home.jsx";
 import Transparency from "./pages/Transparency.jsx";
