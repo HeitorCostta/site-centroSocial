79e47dc6ca2d1dc14f4b4a9904103d7cd776f9fd
HeitorCostta <heitor.fdcosta@gmail.com>
2026-05-08 20:36:32 -0300

    att btn

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -21,7 +21,7 @@ export default function Hero() {
       <div className="hero-actions">
 
         <Link to="/doacao" className="btn-primary">
-          ❤️ Transforme Vidas
+          Transforme Vidas
         </Link>
 
       </div>
